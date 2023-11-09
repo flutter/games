53a9c99045616fa86113b9f264937f4244123c3b
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-11-09 12:48:15 +0100

    chore: Add platform specific directories

---
+++ templates/endless_runner/android/app/src/main/kotlin/com/example/endless_runner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.endless_runner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ templates/endless_runner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import path_provider_foundation
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
