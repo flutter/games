25b59fcf938186879716da99bb6829aa269e2ec9
Parker Lougheed <parlough@gmail.com>
2024-04-03 13:07:12 -0500

    Update and clean up basic template

--- templates/basic/test/smoke_test.dart
+++ templates/basic/test/smoke_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:basic/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('smoke test', (tester) async {
