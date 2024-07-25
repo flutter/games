d915f9b6afb8a7547fecc39e80e1de82b9662c9d
Papa Daco <papadaco@PapaDaco.local>
2024-07-25 12:45:49 -0400

    Housekeeping

--- templates/basic/lib/blocs/blocs.dart
+++ templates/basic/lib/blocs/blocs.dart
@@ -1,2 +1,6 @@
+// Copyright 2024, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 export 'player_progress/player_progress_bloc.dart';
 export 'settings/settings_bloc.dart';
