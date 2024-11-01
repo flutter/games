dff334891ce93b690b022971daddac37344e98da
Rody Davis <rody.davis.jr@gmail.com>
2024-11-01 11:24:53 -0700

    Update idx-template.nix

--- .idx/idx-template.nix
+++ .idx/idx-template.nix
@@ -8,13 +8,13 @@
     ];
     bootstrap = ''
         ${
-          if template == "samples/ads" then "cp -r ${./samples/ads}/* \"$out\""
-          else if template == "samples/crossword" then "cp -r ${./samples/crossword}/* \"$out\""
-          else if template == "samples/multiplayer" then "cp -r ${./samples/multiplayer}/* \"$out\""
-          else if template == "templates/basic" then "cp -r ${./templates/basic}/* \"$out\""
-          else if template == "templates/card" then "cp -r ${./templates/card}/* \"$out\""
-          else if template == "templates/endless_runner" then "cp -r ${./templates/endless_runner}/* \"$out\""
-          else "cp -r ${./templates/basic}/* \"$out\""
+          if template == "samples/ads" then "cp -r ${../samples/ads}/* \"$out\""
+          else if template == "samples/crossword" then "cp -r ${../samples/crossword}/* \"$out\""
+          else if template == "samples/multiplayer" then "cp -r ${../samples/multiplayer}/* \"$out\""
+          else if template == "templates/basic" then "cp -r ${../templates/basic}/* \"$out\""
+          else if template == "templates/card" then "cp -r ${../templates/card}/* \"$out\""
+          else if template == "templates/endless_runner" then "cp -r ${../templates/endless_runner}/* \"$out\""
+          else "cp -r ${../templates/basic}/* \"$out\""
         }
         chmod -R u+w "$out"
     '';
