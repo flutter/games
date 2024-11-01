c8f03d4e96cc9f52484d13fd8ba4963657230e60
Rody Davis <rody.davis.jr@gmail.com>
2024-11-01 12:23:09 -0700

    fix onCreate hook

--- samples/ads/.idx/dev.nix
+++ samples/ads/.idx/dev.nix
@@ -22,6 +22,29 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
+        build-flutter = ''
+          cd /home/user/myapp/android
+
+          ./gradlew \
+            --parallel \
+            -Pverbose=true \
+            -Ptarget-platform=android-x86 \
+            -Ptarget=/home/user/myapp/lib/main.dart \
+            -Pbase-application-name=android.app.Application \
+            -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
+            -Pdart-obfuscation=false \
+            -Ptrack-widget-creation=true \
+            -Ptree-shake-icons=false \
+            -Pfilesystem-scheme=org-dartlang-root \
+            assembleDebug
+
+          # TODO: Execute web build in debug mode.
+          # flutter run does this transparently either way
+          # https://github.com/flutter/flutter/issues/96283#issuecomment-1144750411
+          # flutter build web --profile --dart-define=Dart2jsOptimization=O0 
+
+          adb -s localhost:5555 wait-for-device
+        '';
         installDependencies = "flutter pub get";
       };
     };
@@ -30,7 +53,7 @@
       enable = true;
       previews = {
         android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "emulator-5554"];
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
           manager = "flutter";
         };
       };
