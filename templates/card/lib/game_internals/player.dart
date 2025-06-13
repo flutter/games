0804f9aa23f99a2309b724aa908db407e13f5679
Filip Hracek <filip.hracek@gmail.com>
2025-06-13 11:27:22 +0200

    Apply dart format

--- templates/card/lib/game_internals/player.dart
+++ templates/card/lib/game_internals/player.dart
@@ -5,8 +5,10 @@ import 'playing_card.dart';
 class Player extends ChangeNotifier {
   static const maxCards = 7;
 
-  final List<PlayingCard> hand =
-      List.generate(maxCards, (index) => PlayingCard.random());
+  final List<PlayingCard> hand = List.generate(
+    maxCards,
+    (index) => PlayingCard.random(),
+  );
 
   void removeCard(PlayingCard card) {
     hand.remove(card);
